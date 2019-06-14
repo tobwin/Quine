1e62d486d8c736c69d640eca9933bdcab30c1186
Tobias Winner <winner.tobias@gmail.com>
2019-06-14 13:18:48 +0200

    Update Quine.java
    Switched to switch

--- Quine.java
+++ Quine.java
@@ -11,18 +11,19 @@ public class Quine {
                 "    private static String quote(String s) {\n" +
                 "        StringBuilder q = new StringBuilder();\n" +
                 "        for(int i = 0; i < s.length(); i++) {\n" +
-                "            if(s.charAt(i) == '\\n') {\n" +
-                "                q.append(\"\\\\n\\\" +\\n                \\\"\");\n" +
-                "            } else if(s.charAt(i) == '\\\"') {\n" +
-                "                q.append(\"\\\\\\\"\");\n" +
-                "            } else if(s.charAt(i) == '\\\\') {\n" +
-                "                q.append(\"\\\\\\\\\");\n" +
-                "            } else {\n" +
-                "                q.append(s.charAt(i));\n" +
+                "            \n" +
+                "            switch(s.charAt(i)) {\n" +
+                "                case '\\n': { q.append(\"\\\\n\\\" +\\n                \\\"\"); break; }\n" +
+                "                case '\\\"': { q.append(\"\\\\\\\"\"); break; }\n" +
+                "                case '\\\\': { q.append(\"\\\\\\\\\"); break; }\n" +
+                "                default: q.append(s.charAt(i));\n" +
                 "            }\n" +
+                "            \n" +
                 "        }\n" +
                 "        return q.toString();\n" +
                 "    }\n" +
+                "\n" +
+                "    // INSERT CODE HERE\n" +
                 "\n" +
                 "}";
         System.out.println(s.substring(0,87) + quote(s) +  s.substring(87));
@@ -31,17 +32,18 @@ public class Quine {
     private static String quote(String s) {
         StringBuilder q = new StringBuilder();
         for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '\n') {
-                q.append("\\n\" +\n                \"");
-            } else if(s.charAt(i) == '\"') {
-                q.append("\\\"");
-            } else if(s.charAt(i) == '\\') {
-                q.append("\\\\");
-            } else {
-                q.append(s.charAt(i));
+
+            switch(s.charAt(i)) {
+                case '\n': { q.append("\\n\" +\n                \""); break; }
+                case '\"': { q.append("\\\""); break; }
+                case '\\': { q.append("\\\\"); break; }
+                default: q.append(s.charAt(i));
             }
+
         }
         return q.toString();
     }
+
+    // INSERT CODE HERE
 
 }
